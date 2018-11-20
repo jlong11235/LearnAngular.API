94be675d1dd16ed5511340c58146f845712948c7
jlong11235 <jlong11235@gmail.com>
2018-11-20 20:09:30 +0000

    adding authentication

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -20,38 +19,19 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
         
-        // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
-            var values =  await _context.Values.ToListAsync();
-            return Ok(values);
+            var results = await _context.Values.ToListAsync();
+            return Ok(results);
         }
-
-        // GET api/values/5
+        
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValues(int id)
         {
-            var value = await _context.Values.SingleOrDefaultAsync(x => x.Id == id);
-            return Ok(value);
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var results = await _context.Values.SingleOrDefaultAsync(x => x.Id == id);
+            return Ok(results);
         }
     }
 }
